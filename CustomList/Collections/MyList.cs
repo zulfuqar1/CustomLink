c9cb30f8dfc451af6a8d6c955136931b49b69f8a
zulfuqar1 <zulfugarga@code.edu.az>
2023-04-02 22:36:49 +0400

    AddRange(), Clear() ,Reverse(),Remove() Added

---
+++ CustomList/Collections/MyList.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Windows.Markup;
+
+namespace CustomList.Collections;
+
+public class MyList<T>:IEnumerable<T>
+{
+    public int Count { get; private set; }
+    private int _capacity;
+    public int Capacity 
+    {
+        get => _capacity;
+        set
+        {
+            if (value < Count)
+            {
+                throw new ArgumentOutOfRangeException("capacity was less than the current size.");
+            }
+            _capacity = value;
+            Array.Resize(ref array, _capacity);
+        }
+    }
+    private T[] array;
+    private IEnumerable<T> values;
+
+    public MyList()
+    {
+        Count = 0;
+        _capacity = 0;
+        array = new T[_capacity];
+    }
+    public T this[int index] { 
+        get
+        {
+            if (index >= Count) 
+            { 
+                throw new ArgumentOutOfRangeException(); 
+            }
+            return array[index];
+        } 
+        set
+        {
+            if (index >= Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            array[index] = value;
+        } 
+    }
+
+    public void Add(T obj)
+    {
+        if (_capacity == 0)
+        {
+            _capacity = 4;
+            Array.Resize(ref array, _capacity);
+        }
+        if (_capacity == Count)
+        {
+            _capacity *= 2;
+            Array.Resize(ref array, _capacity);
+        }
+        array[Count]=obj;
+        Count++;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            yield return array[i];
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public bool Contains(T obj)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (obj.Equals(array[i]))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public T? Find(Predicate<T> predicate)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (predicate(array[i]))
+            {
+                return array[i];
+            }
+        }
+        return default;
+    }
+    //Remove()
+    public bool Remove(T obj)
+    {
+        for (int i = 0; i< Count; i++)
+        { 
+            if (obj.Equals(array[i]))
+            {
+                array[i] = default;break;
+            }
+        }
+        return false;
+    }
+    //reverse()
+    public class reverse
+    {
+        public static void Main()
+        {
+            List<int> list = new List<int>{1, 2, 3, 4, 5 };
+            
+            List<int> reverse = Enumerable.Reverse(list).ToList();
+
+            Console.WriteLine(string.Join(',',reverse)); 
+        }
+    }
+    //clear()
+    public void clear()
+    {
+        Capacity= 0;
+        Count= 0;
+        array = default;
+    }
+    //Addrange()
+    public void AddRange(IEnumerable<T> list)
+    {
+        foreach(T item in values)
+        {
+            Add(item);
+        }
+    }
+}
---
+++ CustomList/Program.cs
@@ -0,0 +1,49 @@
+﻿using CustomList.Collections;
+
+List<int> list = new List<int>();
+
+list.Add(1);
+list.Add(1);
+list.Add(2);
+list.Capacity = 3;
+Console.WriteLine(list.Find(n => n == 0));
+
+//list.Add(2);
+//Console.WriteLine(list.Capacity);
+////foreach (int i in List)
+////{
+////    Console.WriteLine(i);
+////}
+//Console.WriteLine(List.Contains());
+
+//MyList<int> myList = new MyList<int>();
+//myList.Capacity = 5;
+//myList.Add(1);
+//myList.Add(1);
+//myList.Add(2);
+//Console.WriteLine(myList.Find(n=>n==0));
+//Console.WriteLine(myList.Capacity);
+//foreach (var item in myList)
+//{
+//    Console.WriteLine(item);
+//}
+//myList.Capacity = 3;
+//Console.WriteLine(myList.Contains(5));
+
+
+
+//IEnumerable<int> GetEvenNumbers(int[] arr)
+//{
+//	foreach (var item in arr)
+//	{
+//		if (item % 2 == 0)
+//			yield return item;
+//	}
+//}
+
+//IEnumerable<int> res = GetEvenNumbers(new int[] { 1, 2, 3, 4,5,6,7 });
+
+//foreach (var item in res)
+//{
+//	Console.WriteLine(item);
+//}
